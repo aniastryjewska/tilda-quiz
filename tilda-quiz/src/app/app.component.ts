04cd72a94806a5b785b85cee9b09af820bdf2eeb
ania stryjewska <ania.stryjewska@gmail.com>
2021-05-08 17:11:29 +0200

    graphql querying first test

--- tilda-quiz/src/app/app.component.ts
+++ tilda-quiz/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+interface Quiz {
+  id: string;
+  name: string;
+}
+
+interface Response {
+  quizzes: Quiz[];
+}
+
+const GET_QUIZZES = gql`
+query Quizzes {
+  quizzes {
+    id
+    name
+  }
+}
+`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tilda-quiz';
+
+  quizzes$: Observable<Quiz[]>;
+
+  constructor(private apollo:Apollo) {}
+
+  ngOnInit(): void {
+    this.quizzes$ = this.apollo.watchQuery<Response>({
+query: GET_QUIZZES
+    }).valueChanges.pipe(map(result => result.data.quizzes))
+  }
 }
