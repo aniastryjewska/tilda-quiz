5655be062fbad3fd42c2e6e139ee39b5080fda6a
ania stryjewska <ania.stryjewska@gmail.com>
2021-05-09 20:56:00 +0200

    finalized quiz interface

--- tilda-quiz/src/app/interfaces/quiz.ts
+++ tilda-quiz/src/app/interfaces/quiz.ts
@@ -1,4 +1,12 @@
 export interface Quiz {
     id: string;
     name: string;
+    questions: {
+      answer: string;
+      id: string;
+      options: string;
+      text: string;
+    }
   }
+
+ 
